7cd6f55c712267a16002861b838441be5321af8d
Harry <harrydeng2008@gmail.com>
2018-05-11 07:59:22 +0800

    getting strated

--- app/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import {render} from 'react-dom';
-import Hello from './hello';
-import './index.css';
-
-render(<Hello />, document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,9 @@
-const webpack = require('webpack');
+const path = require('path');
 
 module.exports = {
-  devtool: 'eval-source-map',
-
-  entry: __dirname + '/app/index.js',
+  entry: './src/index.js',
   output: {
-    path: __dirname + '/public',
-    filename: 'bundle.js'
-  },
-
-  devServer: {
-    contentBase: './public',
-    port: '9000',
-    historyApiFallback: true,
-    inline: true
-  },
-
-  module: {
-    rules: [
-      {
-        test: /(\.jsx|\.js)$/,
-        use: {
-          loader: 'babel-loader'
-        },
-        exclude: /node_modules/
-      },
-      {
-        test: /\.css$/,
-        use: [
-          {
-            loader: "style-loader"
-          },
-          {
-            loader: "css-loader",
-            options: {
-              modules: true,
-              localIdentName: '[name]__[local]--[hash:base64:5]'
-            }
-          },
-          {
-            loader: "postcss-loader"
-          }
-        ]
-      }
-    ]
-  },
-  plugins: [
-    new webpack.BannerPlugin('Hello World')
-  ]
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  }
 }
